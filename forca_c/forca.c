5793ac7809fee87a8ef400bb3b3ac0c735d90820
Bernardo Win <berdfandrade@gmail.com>
2022-08-19 18:49:18 -0300

    Adicionando arquivos

--- forca_c/forca.c
+++ forca_c/forca.c
@@ -4,7 +4,23 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
+void abertura(){
+    printf("****************************\n");
+    printf("**      JOGO DA FORCA     **\n");
+    printf("****************************\n\n");
+}
+
+void chuta(char chutes[26], int tentativas) { // Lembrando que esse tentativas, não vai representar o tentativas lá de baixo... As variáveis só existem no contexto em que elas foram declaradas. 
+
+    char chute;
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+
+}
+
+int main() {
+
     char palavrasecreta[20];
     sprintf(palavrasecreta, "MELANCIA");
     
@@ -14,14 +30,13 @@ int main(){
     char chutes[26];
     int tentativas = 0;
 
+    abertura(); // Assim eu invoco a função abertura; tipo javascript... 
+
 
     do {
         for(int i = 0; i < strlen(palavrasecreta); i++){
-
-            int achou = 0;
-            
+            int achou = 0;            
             for(int j = 0; j < tentativas; j++) {
-
                 if(chutes[j] == palavrasecreta[i]){
                     achou = 1;
                     break;
@@ -37,10 +52,7 @@ int main(){
         }
         printf("\n"); // criando nova linha. 
 
-        char chute;
-        scanf(" %c", &chute);
-
-        chutes[tentativas] = chute;
+        chuta(chutes, tentativas);
         tentativas++;
 
     } while (!acertou && !enforcou); 
---
+++ forca_c/tabuada.c
@@ -0,0 +1,19 @@
+
+
+#include <stdio.h>
+
+void abertura(int m){ // Para a função abertura ser invocada, precisamos passar para ela, um inteiro; 
+
+    printf("Tabuada do %d\n\n", m);
+}
+
+int main() {
+
+    int multiplicador = 2;
+
+    abertura(multiplicador);
+
+    for(int i = 1; i<= 10; i++){
+        printf("%d X %d = %d\n", multiplicador, i, multiplicador * i );
+    }
+}
