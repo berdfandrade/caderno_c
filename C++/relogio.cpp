363abe9bb1f62b5c986775856566ad2cf0c32545
berdfandrade1 <berdfandrade@gmail.com>
2022-10-18 09:39:32 -0300

    prática

--- C++/relogio.cpp
+++ C++/relogio.cpp
@@ -103,8 +103,66 @@ void printDigit(int no, int x, int y){
                 case 0:
                     gotoxy(x + j, y + i); cout << d0[i][j]; break; 
                 case 1:
-                    gotoxy(x + j) // minuto 2:47 Digital CLock project in c++
+                    gotoxy(x + j, y + j); cout << d1[i][j]; break; 
+				case 2: 
+					gotoxy(x + j, y + j); cout << d2[i][j]; break;
+				case 3: 
+					gotoxy(x + j, y + j); cout << d3[i][j]; break;
+				case 4:
+					gotoxy(x + j, y + j); cout << d4[i][j]; break;
+				case 5:
+					gotoxy(x + j, y + j); cout << d5[i][j]; break;
+				case 6:
+					gotoxy(x + j, y + j); cout << d6 [i][j]; break;
+				case 7:
+					gotoxy(x + j, y + j); cout << d7 [i][j]; break;
+				case 8:
+					gotoxy(x + j, y + j); cout << d8 [i][j]; break;
+				case 9:
+					gotoxy(x + j, y + j); cout << d9 [i][j]; break;
+				case 10
+					gotoxy(x + j, y + j); cout << d10 [i][j]; break;						
             }
         }
     }
+}
+
+void drawBorder(int x = 0, int y = 0){
+	char v = 186; // Vertical 
+	char h = 205; // Horizontal
+	char tr = 187; 
+	char br = 188;  
+	char tl = 201; 
+	char bl = 200; 
+	int width = 50; 
+	int height = 11;  
+
+	gotoxy((width/2)+x-11, y): cout << "C U R R E N T   T I M E";
+
+
+	for(int i = 1; i < height; i++){
+		for(int j = 1; j < width; j++){
+			gotoxy(j+x, i+x); 
+			if(i == 1 && j == 1)
+			cout << tl; 
+			else if (i == height && j == 1 )
+			cout << bl;
+			else if (i == 1 && j == width) 
+			cout << tr; 
+			else if (i = height && j == width)
+			cout << br;
+			else if (i == 1 || i == height) 
+			cout << h;
+			else if (j == 1 || j == width)
+			cout << v;  
+		}
+	}
+}
+
+int main(){
+	system("cls");
+	setcursor(0, 0);
+	srand((unsigned) time(NULL)); 
+
+	time_t now = time(0)
 }
