a64a4881886109c23830b42a19034a08f8efc0c0
Bernardo Win <berdfandrade@gmail.com>
2022-10-26 14:41:46 -0300

    Adicionando coisas novas

---
+++ C++/adv.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+
+int main () {
+
+    const int NUMERO_SECRETRO = 45;
+    bool nao_acertou = true;
+    int tentativas = 0; 
+
+
+    while (nao_acertou){ 
+
+    int chute;
+    cout << "Qual é o valor do seu chute? ";
+    cin >> chute;
+    cout << "O valor do chute é " << chute << endl; 
+
+    
+    bool acertou = chute == NUMERO_SECRETRO; 
+    bool maior = chute > NUMERO_SECRETRO;
+
+            
+    if(acertou){
+        cout << "Parabéns feller" << endl;
+        cout << "Acabou!" << endl;
+        tentativas++;
+        nao_acertou = false;
+        // Mensagem de fim de jogo. 
+        cout << "Você acertou em " << tentativas << "tentativas" << endl; 
+    } else if (maior){
+        cout << "Seu chute foi maior que o número secreto" << endl;
+        tentativas++;
+        cout << "Suas tentativas foram: " << tentativas << endl; 
+
+    } else {
+        cout << "Seu chute foi menor que o número secreto" << endl;
+        tentativas++;
+        cout << "Suas tentativas foram: " << tentativas << endl; 
+    }
+    
+} 
+
+}
