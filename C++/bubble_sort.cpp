46d248418e87472e7bc01ac414c690f533652cc3
Bernardo Win <berdfandrade@gmail.com>
2022-10-28 00:08:58 -0300

    Escrevendo

---
+++ C++/bubble_sort.cpp
@@ -0,0 +1,35 @@
+
+#include<iostream>
+using namespace std;
+int main ()
+{
+   int i, j,temp,pass=0;
+   int a[10] = {10,2,0,14,43,25,18,1,5,45};
+
+   cout <<"Input list ...\n";
+
+   for(i = 0; i<10; i++) {
+      cout <<a[i]<<"\t"; // Passa pela lista, e imprime cada número que vai ser percorrido no array...
+   }
+cout<<endl; // fim da linha
+
+for(i = 0; i<10; i++) { // Vai percorrer o array de 10 números
+   for(j = i+1; j<10; j++) // Vai percorrer o array da lista, e um número que está na frente dele. até terminarmos o array de 10 números
+   {
+      if(a[j] < a[i]) { // Se o número J, que é o número da frente do array, 
+         temp = a[i]; // for menor que o número de trás, Colocamos o a[i], em uma variável temporária... 
+         a[i] = a[j]; // o próximo número vira o anterior, portanto, ele vai para trás na lista, fazendo com que fique em ordem crescente.
+         a[j] = temp; // temp vira o maior número... 
+      }
+   }
+pass++; // Pass vai aumentando de acordo com a quantidade de tentativas para fazer o sort
+}
+
+cout <<"Sorted Element List ...\n"; // imprime a lista que foi feita pelo bubble sort. 
+for(i = 0; i<10; i++) {
+   cout <<a[i]<<"\t";
+}
+
+cout<<"\nNumber of passes taken to sort the list:"<<pass<<endl; // pegamos o pass que foi a quantidade de vezes que o loop teve que repetir para conseguir fazer o sort. 
+return 0;
+}
